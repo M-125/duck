77f3f2c766c0057386126df583034c14f5de0cc5
M-125 <kaman.marcell.2009.10.28@gmail.com>
2024-06-13 15:04:09 +0200

    mapsace

--- scripts/Noise.gd
+++ scripts/Noise.gd
@@ -2,14 +2,24 @@ extends OpenSimplexNoise
 class_name Noise
 var replace=[]
 var file=File.new()
+func _init():
+	Global.connect("save",self,"save")
 func get_noise_2d(x,y):
-	if file.file_exists("user://noise"+str(self.seed)):
+	
+	if not file.file_exists("user://noise"+str(self.seed)):
 		file.open("user://noise"+str(self.seed),File.WRITE)
 		for e in range(Global.mapsize):for i in range(Global.mapsize):
 			file.store_8(0)
-	
-	var noiseval=abs(get_noise_2dv(Vector2(x,y)))*10
-	
+		file.close()
+		print("created")
+	if not file.is_open():
+		file.open("user://noise"+str(self.seed))
+	print("read")
+	var noiseval
+	if readpos(x,y)==0:
+		writepos(x,y,round(abs(get_noise_2dv(Vector2(x,y)))*10*10))
+	noiseval=readpos(x,y)/10
+	print("ns",noiseval)
 	for e in replace:
 		var rect=e[0]
 		if x>=rect.position.x and x<=rect.end.x and y>=rect.position.y and y<=rect.end.y:
@@ -29,3 +39,12 @@ func get_noise_2d(x,y):
 func addzone(rect:Rect2,req:String):
 	if not [rect,req] in replace:
 		replace.append([rect,req])
+
+func readpos(x,y):
+	file.seek(x+(Global.mapsize*y))
+	return file.get_8()
+
+func writepos(x,y,val):
+	file.seek(x+(Global.mapsize*y))
+	return file.store_8(val)
+			
--- scripts/loadgame.gd
+++ scripts/loadgame.gd
@@ -21,8 +21,27 @@ func new():
 	get_tree().change_scene("res://scenes/map2.0.tscn")
 	Global.savemap=null
 	Global.playerpack=null
+	var dir=Directory.new()
+	for e in dir_contents("user://"):
+		if "noise" in e:
+			dir.remove(e)
 func Load():
 	Global.savemap=load("user://save.tscn")
 	Global.playerpack=load("user://saveplayer.tscn")
 	Global.small_stuff=Saver.load("user://coins.json")
 	get_tree().change_scene_to(Global.savemap)
+
+func dir_contents(path):
+	var dir = Directory.new()
+	var files=[]
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				pass
+			else:
+				files.append(path+file_name)
+			file_name = dir.get_next()
+	return files
+																															
